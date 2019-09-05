457c82334ad5000ca4e8d00f02a1f276bd3491c3
Shitao Chen <terrychanst@gmail.com>
2019-09-05 18:38:26 -0500

    Add three unit test

--- src/test/YourUnitTests.java
+++ src/test/YourUnitTests.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.NoSuchElementException;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,7 +16,10 @@ import hw1.Database;
 import hw1.HeapFile;
 import hw1.HeapPage;
 import hw1.TupleDesc;
-
+import hw1.Type;
+/*
+ * @author Shitao Chen
+ */
 public class YourUnitTests {
 	
 	private HeapFile hf;
@@ -46,5 +50,38 @@ public class YourUnitTests {
 	public void test() {
 		fail("Not yet implemented");
 	}
+
+	@Test
+	public void testNumFields_TupleDesc() {
+		Type[] t = {Type.INT, Type.INT, Type.INT};
+		String[] c = {"", "", ""};
+		TupleDesc td = new TupleDesc(t, c);
+		assertTrue(td.numFields() == 3);
+	}
+	
+	@Test
+	public void testGetFieldName_TupleDesc_HappyCase() {
+		Type[] t = {Type.INT, Type.INT, Type.INT};
+		String[] c = {"field1", "field2", "field3"};
+		TupleDesc td = new TupleDesc(t, c);
+		
+		assertTrue(td.getFieldName(0) == c[0]);
+	}
+	
+	@Test
+	public void testGetFieldName_TupleDesc_Exception() {
+		Type[] t = {Type.INT, Type.INT, Type.INT};
+		String[] c = {"field1", "field2", "field3"};
+		TupleDesc td = new TupleDesc(t, c);
+		Boolean flag = false; 
+		
+		try {
+			td.getFieldName(5);
+		} catch (NoSuchElementException e) {
+			flag = true;
+		}
+		
+		assertTrue(flag);
+	} 
 
 }

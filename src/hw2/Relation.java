db17798a9633e67e4c4f2bafaca8dc4289058a30
Shitao Chen <terrychanst@gmail.com>
2019-09-28 17:01:27 -0500

    Finished Relation

--- src/hw2/Relation.java
+++ src/hw2/Relation.java
@@ -1,16 +1,20 @@
 package hw2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import hw1.Field;
 import hw1.RelationalOperator;
 import hw1.Tuple;
 import hw1.TupleDesc;
+import hw1.Type;
 
 /**
  * This class provides methods to perform relational algebra operations. It will be used
  * to implement SQL queries.
- * @author Doug Shook
+ * @author Shitao Chen
  *
  */
 public class Relation {
@@ -19,7 +23,8 @@ public class Relation {
 	private TupleDesc td;
 	
 	public Relation(ArrayList<Tuple> l, TupleDesc td) {
-		//your code here
+		this.tuples = l;
+		this.td = td;
 	}
 	
 	/**
@@ -31,18 +36,34 @@ public class Relation {
 	 */
 	public Relation select(int field, RelationalOperator op, Field operand) {
 		//your code here
-		return null;
+		ArrayList<Tuple> newTuples = (ArrayList<Tuple>) this.tuples.stream()
+		.filter(e -> e.getField(field).compare(op, operand))
+		.collect(Collectors.toList());;
+		return new Relation(newTuples, this.td);
 	}
-	
 	/**
 	 * This method performs a rename operation on a relation
 	 * @param fields the field numbers (refer to TupleDesc) of the fields to be renamed
 	 * @param names a list of new names. The order of these names is the same as the order of field numbers in the field list
 	 * @return
 	 */
-	public Relation rename(ArrayList<Integer> fields, ArrayList<String> names) {
-		//your code here
-		return null;
+	public Relation rename(ArrayList<Integer> fields, ArrayList<String> names) throws Exception {
+		Type[] types = new Type[this.td.numFields()];
+		String[] field = new String[this.td.numFields()];
+		
+		for(int i = 0; i < this.td.numFields(); i++) {
+			types[i] = this.td.getType(i);
+			field[i] = this.td.getFieldName(i);
+		}
+		for(int i = 0; i < fields.size(); i++) {
+			if(names.get(i) != "")
+				field[fields.get(i)] = names.get(i);
+		}
+		Set<String> set = Arrays.stream(field).collect(Collectors.toSet());
+		if(set.size() != this.td.numFields())
+			throw new Exception();
+		
+		return new Relation(this.tuples, new TupleDesc(types, field));
 	}
 	
 	/**
@@ -51,8 +72,26 @@ public class Relation {
 	 * @return
 	 */
 	public Relation project(ArrayList<Integer> fields) {
-		//your code here
-		return null;
+		Type[] types = new Type[fields.size()];
+		String[] field = new String[fields.size()];
+		for(int i = 0; i < fields.size(); i++) {
+			types[i] = this.td.getType(fields.get(i));
+			field[i] = this.td.getFieldName(fields.get(i));
+		}
+		
+		TupleDesc newTd = new TupleDesc(types, field);
+		
+		ArrayList<Tuple> newTupleList = new ArrayList<>();
+		if(newTd.getSize() == 0)
+			return new Relation(newTupleList, newTd);
+		for(Tuple tuple: this.tuples) {
+			Tuple e = new Tuple(newTd);
+			int n = 0;
+			for(int j = 0; j < fields.size(); j++)
+				e.setField(n++, tuple.getField(fields.get(j)));
+			newTupleList.add(e);
+		}
+		return new Relation(newTupleList, newTd);
 	}
 	
 	/**
@@ -65,8 +104,40 @@ public class Relation {
 	 * @return
 	 */
 	public Relation join(Relation other, int field1, int field2) {
-		//your code here
-		return null;
+		int numField1 = this.td.numFields();
+		int numField2 = other.getDesc().numFields();
+		Type[] types = new Type[numField1 + numField2];
+		String[] field = new String[numField1 + numField2];
+		for(int i = 0; i < numField1; i++) {
+			types[i] = this.td.getType(i);
+			field[i] = this.td.getFieldName(i);
+		}
+		for(int i = 0; i < numField2; i++) {
+			types[numField1 + i] = other.getDesc().getType(i);
+			field[numField1 + i] = other.getDesc().getFieldName(i);
+		}
+		TupleDesc newTd = new TupleDesc(types, field);
+		ArrayList<Tuple> cartesianProduct  = new ArrayList<>();
+		for(Tuple tuple1: this.tuples)
+			for(Tuple tuple2: other.getTuples()) {
+				Tuple tmp = new Tuple(newTd);
+				for(int i = 0; i < numField1; i++)
+					tmp.setField(i, tuple1.getField(i));
+				for(int i = 0; i < numField2; i++)
+					tmp.setField(i + numField1, tuple2.getField(i));
+				cartesianProduct.add(tmp);
+			}
+		
+		ArrayList<Tuple> newTuples = (ArrayList<Tuple>) cartesianProduct
+				.stream()
+				.filter(e -> e
+						.getField(field1)
+						.compare(RelationalOperator.EQ,
+									e.getField(field2 + numField1)
+								)
+					    )
+				.collect(Collectors.toList());
+		return new Relation(newTuples, newTd);
 	}
 	
 	/**
@@ -76,18 +147,18 @@ public class Relation {
 	 * @return
 	 */
 	public Relation aggregate(AggregateOperator op, boolean groupBy) {
-		//your code here
-		return null;
+		Aggregator agg = new Aggregator(op, groupBy, this.td);
+		for(Tuple tp: this.tuples)
+			agg.merge(tp);
+		return new Relation(agg.getResults(), this.td);
 	}
 	
 	public TupleDesc getDesc() {
-		//your code here
-		return null;
+		return this.td;
 	}
 	
 	public ArrayList<Tuple> getTuples() {
-		//your code here
-		return null;
+		return this.tuples;
 	}
 	
 	/**
@@ -95,7 +166,9 @@ public class Relation {
 	 * first contain the TupleDesc, followed by each of the tuples in this relation
 	 */
 	public String toString() {
-		//your code here
-		return null;
+		String ret = this.td.toString() + " ";
+		for(int i = 0; i < this.tuples.size(); i++)
+			ret += this.tuples.get(i).toString();
+		return ret;
 	}
 }

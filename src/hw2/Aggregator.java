dbc20dfb03dcb10a0f274450d7c96da170236264
Shitao Chen <terrychanst@gmail.com>
2019-10-02 17:13:01 -0500

    Aggregator non-group

--- src/hw2/Aggregator.java
+++ src/hw2/Aggregator.java
@@ -1,15 +1,20 @@
 package hw2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 
+import hw1.IntField;
+import hw1.RelationalOperator;
+import hw1.StringField;
 import hw1.Tuple;
 import hw1.TupleDesc;
+import hw1.Type;
 
 /**
  * A class to perform various aggregations, by accepting one tuple at a time
- * @author Doug Shook
+ * @author Shitao Chen & Junji Heng
  *
  */
 public class Aggregator {
@@ -17,11 +22,37 @@ public class Aggregator {
 	private TupleDesc td;
 	private boolean groupBy;
 	private AggregateOperator o;
+	private HashMap<IntField, Integer> countMap_Int = new HashMap<>(); 
+	private HashMap<StringField, Integer> countMap_Str = new HashMap<>(); 
+	private IntField constantTmp;
 	public Aggregator(AggregateOperator o, boolean groupBy, TupleDesc td) {
 		this.o = o;
 		this.groupBy = groupBy;
 		this.td = td;
 		this.tuples = new ArrayList<Tuple>();
+		
+		tuples.add(new Tuple(td));
+		switch(o){
+			case COUNT:
+				this.tuples.get(0).setField(0, new IntField(0));
+				break;
+			case MAX:
+				this.tuples.get(0).setField(0, new StringField("a"));
+				if(td.getType(0) == Type.INT)
+					this.tuples.get(0).setField(0, new IntField(Integer.MIN_VALUE));
+				break;
+			case MIN:
+				this.tuples.get(0).setField(0, new StringField("z"));
+				if(td.getType(0) == Type.INT)
+					this.tuples.get(0).setField(0, new IntField(Integer.MAX_VALUE));
+				break;
+			case AVG:
+			case SUM:
+				this.tuples.get(0).setField(0, new IntField(0));
+				break;
+		}
+		constantTmp = new IntField(0);
+		countMap_Int.put(constantTmp, 0);
 	}
 
 	/**
@@ -29,17 +60,66 @@ public class Aggregator {
 	 * @param t the tuple to be aggregated
 	 */
 	public void merge(Tuple t) {
-		//your code here
-		
+		if(this.groupBy) {
+			merge_Group(t);
+			return;
+		}
+		switch(o){
+			case COUNT:
+				IntField times = (IntField) this.tuples.get(0).getField(0);
+				this.tuples.get(0).setField(0, new IntField(times.getValue() + 1));
+				break;
+			case MAX:
+				if(!this.tuples.get(0).getField(0).compare(RelationalOperator.GT, t.getField(0))) {
+					this.tuples.get(0).setField(0, t.getField(0));
+				}
+				break;
+			case MIN:
+				if(this.tuples.get(0).getField(0).compare(RelationalOperator.GT, t.getField(0))) {
+					this.tuples.get(0).setField(0, t.getField(0));
+				}
+				break;
+			case AVG:
+			case SUM:
+				IntField sums = (IntField) this.tuples.get(0).getField(0);
+				this.tuples.get(0).setField(0, new IntField(sums.getValue() + ((IntField) t.getField(0)).getValue()));
+				countMap_Int.put(constantTmp, countMap_Int.get(constantTmp) + 1);
+				break;
+		}
 	}
+	
+	private void merge_Group(Tuple t) {
+		switch(o){
+			case COUNT:
+				break;
+			case MAX:
+				break;
+			case MIN:
+				break;
+			case AVG:
+			case SUM:
+				break;
+		}
+	}
+	
 	
 	/**
 	 * Returns the result of the aggregation
 	 * @return a list containing the tuples after aggregation
 	 */
 	public ArrayList<Tuple> getResults() {
-		//your code here
+		if(this.groupBy)
+			return this.getResults_Group();
+		
+		if(o == AggregateOperator.AVG) {
+			IntField sums = (IntField) this.tuples.get(0).getField(0);
+			int count = countMap_Int.get(constantTmp);
+			this.tuples.get(0).setField(0, new IntField(sums.getValue() / count));
+		}
+		return this.tuples;
+	}
+	private ArrayList<Tuple> getResults_Group(){
 		return null;
 	}
-
+	
 }

95c5c5baf0c1eea7c5a36bcec0623a4048e5a39e
Shitao Chen <terrychanst@gmail.com>
2019-10-08 18:02:46 -0500

    hw3 test

--- src/test/YourHW3Tests.java
+++ src/test/YourHW3Tests.java
@@ -2,12 +2,18 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
+import hw1.Field;
 import hw1.IntField;
+import hw1.RelationalOperator;
 import hw3.BPlusTree;
 import hw3.Entry;
 import hw3.InnerNode;
+import hw3.LeafNode;
+import hw3.Node;
 
 public class YourHW3Tests {
 
@@ -18,11 +24,30 @@ public class YourHW3Tests {
 		assertTrue(bt.getRoot().isLeafNode() == true);
 		assertTrue(((InnerNode)bt.getRoot()).getChildren().size() == 0);
 	}
+	
 	@Test
 	public void testMoreDegree() {
 		BPlusTree bt = new BPlusTree(4, 3);
-		bt.insert(new Entry(new IntField(1), 0));
+		bt.insert(new Entry(new IntField(1), 9));
+		bt.insert(new Entry(new IntField(1), 4));
+		bt.insert(new Entry(new IntField(1), 2));
+		//verify root properties
+		Node root = bt.getRoot();
+		assertTrue(root.isLeafNode() == true);
 		
+		bt.insert(new Entry(new IntField(1), 7));
+		InnerNode in = (InnerNode)bt.getRoot();
+		
+		assertTrue(in.isLeafNode() == false);
+		assertTrue(in.getChildren().size() == 2);
+		
+		//grab left and right children from root
+		LeafNode l = (LeafNode)in.getChildren().get(0);
+		LeafNode r = (LeafNode)in.getChildren().get(0);
+		assertTrue(l.isLeafNode() == true);
+		assertTrue(r.isLeafNode() == true);
+		assertTrue(l.getEntries().get(0).getField().compare(RelationalOperator.EQ, new IntField(4)));
+		assertTrue(r.getEntries().get(0).getField().compare(RelationalOperator.EQ, new IntField(9)));
 	}
 
 }

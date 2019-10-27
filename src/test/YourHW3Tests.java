c0c122e45fac1c178c99f22de01316832cb30c69
衡俊吉 <bibby@hengjunjideMacBook-Pro.local>
2019-10-27 00:58:22 -0500

    fix my hw3_unit test

--- src/test/YourHW3Tests.java
+++ src/test/YourHW3Tests.java
@@ -22,65 +22,20 @@ public class YourHW3Tests {
 		BPlusTree bt = new BPlusTree(3, 2);
 		bt.insert(new Entry(new IntField(9), 0));
 		assertTrue(bt.getRoot().isLeafNode() == true);
-		assertTrue(((InnerNode)bt.getRoot()).getChildren().size() == 0);
 	}
 	
-	@Test
-	public void testD() {
-		BPlusTree bt = new BPlusTree(3, 2);
-		bt.insert(new Entry(new IntField(9), 0));
-		bt.insert(new Entry(new IntField(4), 0));
-		bt.insert(new Entry(new IntField(12), 0));
-		bt.insert(new Entry(new IntField(13), 0));
-		bt.insert(new Entry(new IntField(15), 0));
-//		bt.insert(new Entry(new IntField(6), 0));
-//		bt.insert(new Entry(new IntField(1), 0));
-//		bt.insert(new Entry(new IntField(3), 0));
-//		bt.insert(new Entry(new IntField(10), 0));
-
-		bt.delete(new Entry(new IntField(18), 0));
-//		bt.delete(new Entry(new IntField(4), 0));
-//		bt.delete(new Entry(new IntField(4), 0));
-//		bt.delete(new Entry(new IntField(10), 0));
-//		bt.delete(new Entry(new IntField(2), 0));
-
-		//verify root properties
-		Node root = bt.getRoot();
-
-		assertTrue(root.isLeafNode() == false);
-		InnerNode in = (InnerNode)root;
-
-		ArrayList<Field> k = in.getKeys();
-		ArrayList<Node> c = in.getChildren();
-
-		assertTrue(k.get(0).compare(RelationalOperator.EQ, new IntField(9)));
-		
-		Node ll = c.get(0);
-		Node lr = c.get(1);
-
-		assertTrue(ll.isLeafNode());
-		assertTrue(lr.isLeafNode());
-		
-		LeafNode lll = (LeafNode)ll;
-		LeafNode lrl = (LeafNode)lr;
-
-		ArrayList<Entry> ell = lll.getEntries();
-
-		assertTrue(ell.get(0).getField().equals(new IntField(4)));
-
-	}
 	
 	@Test
 	public void testMoreDegree() {
 		BPlusTree bt = new BPlusTree(4, 3);
-		bt.insert(new Entry(new IntField(1), 9));
-		bt.insert(new Entry(new IntField(1), 4));
-		bt.insert(new Entry(new IntField(1), 2));
+		bt.insert(new Entry(new IntField(9), 0));
+		bt.insert(new Entry(new IntField(4), 0));
+		bt.insert(new Entry(new IntField(2), 0));
 		//verify root properties
 		Node root = bt.getRoot();
 		assertTrue(root.isLeafNode() == true);
 		
-		bt.insert(new Entry(new IntField(1), 7));
+		bt.insert(new Entry(new IntField(7), 0));
 		InnerNode in = (InnerNode)bt.getRoot();
 		
 		assertTrue(in.isLeafNode() == false);
@@ -88,11 +43,11 @@ public class YourHW3Tests {
 		
 		//grab left and right children from root
 		LeafNode l = (LeafNode)in.getChildren().get(0);
-		LeafNode r = (LeafNode)in.getChildren().get(0);
+		LeafNode r = (LeafNode)in.getChildren().get(1);
 		assertTrue(l.isLeafNode() == true);
 		assertTrue(r.isLeafNode() == true);
-		assertTrue(l.getEntries().get(0).getField().compare(RelationalOperator.EQ, new IntField(4)));
-		assertTrue(r.getEntries().get(0).getField().compare(RelationalOperator.EQ, new IntField(9)));
+		assertTrue(l.getEntries().get(1).getField().compare(RelationalOperator.EQ, new IntField(4)));
+		assertTrue(r.getEntries().get(1).getField().compare(RelationalOperator.EQ, new IntField(9)));
 	}
 
 }
